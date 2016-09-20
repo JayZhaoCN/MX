fb1901fae73b215bccc022e75cc1f2d4a4b2db65
zhaojiabao <zhaojiabao@huami.com>
2016-09-20 17:45:02 +0800

    为什么匿名内部类的参数需要是final的？

--- app/src/main/java/com/jay/mx/activitys/MainActivity.java
+++ app/src/main/java/com/jay/mx/activitys/MainActivity.java
@@ -29,6 +29,18 @@ public class MainActivity extends BaseTitleActivity {
 
         //如何将一个数组转化为List，下面给出了方法：
         final List<String> list = Arrays.asList(getResources().getStringArray(R.array.function_1));
+        //为什么一个匿名内部类的参数必须是final的？下面给出解释：
+        //具体参考这里：http://feiyeguohai.iteye.com/blog/1500108
+        //由于匿名内部类在方法中，方法的生命周期要比匿名内部类的短。
+        //可能出现的状况就是：方法调用完毕，方法中的局部变量也销毁，但是该方法中的匿名内部类还是存在着的。
+        //就会出现一个问题：这个匿名内部类调用了一个已经不存在的变量。
+        //如何解决这个问题：
+        //首先，基本类型是不会出现这个问题的，因为基本类型都是按值传递的。
+        //复杂类型如何解决：
+        //当方法中的局部变量向匿名内部类传参数时，并不是直接按照引用传递，而是在匿名内部类中复制一个局部变量的副本。这样即使局部变量被销毁也无妨，因为现在匿名内部类调用的是局部变量的副本。
+        //但这种解决方案还是会出现一个问题：如果局部变量在复制之后发生修改了怎么办？这样匿名内部类中的副本和修改过得局部变量就不能划等号了，如何解决？
+        //很简单，把这个局部变量定义为final即可。
+        //总结:核心的问题是:怎么才能使得:访问"复制品"与访问真正的原始的局部变量,其语义效果是一样的呢?
 
         MyRecyclerAdapter adapter = new MyRecyclerAdapter(this, list);
         adapter.setOnItemClickListener(new MyRecyclerAdapter.OnItemClickListener() {
