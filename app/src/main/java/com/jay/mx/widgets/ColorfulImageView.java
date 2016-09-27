155a2b5714b59a352b256a7415a58404614c12ce
zhaojiabao <zhaojiabao@smartdevices.com.cn>
2016-09-27 11:34:53 +0800

    变色ImageView尚未解决

--- app/src/main/java/com/jay/mx/widgets/ColorfulImageView.java
+++ app/src/main/java/com/jay/mx/widgets/ColorfulImageView.java
@@ -8,8 +8,10 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.jay.mx.R;
@@ -20,12 +22,12 @@ import com.jay.mx.R;
  */
 
 public class ColorfulImageView extends View {
+    private static final String TAG = "ColorfulImageView";
     private Paint mPaint;
-    private Canvas mCanvas;
-    private Bitmap mBitmap;
     private Rect mIconRect;
     private Context mContext;
-    private int mAlpha = 0;
+    private BitmapDrawable mDrawable;
+    private Bitmap mBitmap;
 
     public ColorfulImageView(Context context) {
         this(context, null);
@@ -37,32 +39,31 @@ public class ColorfulImageView extends View {
 
     public ColorfulImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        mContext = context;
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        mPaint.setColor(ContextCompat.getColor(context, R.color.colorPrimary));
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(mContext.getResources().getColor(R.color.bg_color_red));
         mPaint.setDither(true);
-        mPaint.setAlpha(0);
         mContext = context;
+
+        mDrawable = (BitmapDrawable) mContext.getResources().getDrawable(R.drawable.mm_title_btn_contact_normal);
+        mBitmap = mDrawable.getBitmap();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        mCanvas = new Canvas(mBitmap);
         mIconRect = new Rect(0, 0, w, h);
-        BitmapDrawable drawable = (BitmapDrawable) mContext.getDrawable(R.drawable.mm_title_btn_contact_normal);
-        mBitmap = drawable.getBitmap();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int alpha = (int) Math.ceil((255 * mAlpha));
-        mPaint.setAlpha(alpha);
-        canvas.drawRect(mIconRect, mPaint);
+        Log.i(TAG, mIconRect.toString());
         mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        mPaint.setAlpha(255);
+        canvas.drawRect(mIconRect, mPaint);
         canvas.drawBitmap(mBitmap, null, mIconRect, mPaint);
     }
 }
